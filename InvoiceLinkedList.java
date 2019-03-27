0b93d3be7c06de718dfeb59d22d27dd930d334ed
dks11 <47439162+dks11@users.noreply.github.com>
2019-03-27 19:14:02 -0300

    Update InvoiceLinkedList.java

--- InvoiceLinkedList.java
+++ InvoiceLinkedList.java
@@ -135,10 +135,136 @@ public class InvoiceLinkedList
 
 	public void swapValues(int Index1, int Index2)
 	{	 
+        if (Index1 != Index2)
+		{
+  
+			Node curr1 = head; 
+			while (curr1 != null && curr1.data != getIndex(Index1) ) 
+			{ 
+				curr1 = curr1.next; 
+			} 
+	  
+			Node curr2 = head; 
+			while (curr2 != null && curr2.data != getIndex(Index2) ) 
+			{ 
+				curr2 = curr2.next; 
+			} 
+	  
+				if (curr1 != null || curr2 != null) 
+				{
+					if (curr1.prev != null) 
+						curr1.prev.next = curr2; 
+					else 
+						head = curr2; 
+			  
+					if (curr2.prev != null) 
+						curr2.prev.next = curr1; 
+					else 
+						head = curr1; 
+			  
+					Node temp = curr1.next; 
+					curr1.next = curr2.next; 
+					curr2.next = temp; 
+				}
+		} 
+/*
+	  Node node1 = getNode(Index1);
+	  Node node2 = getNode(Index2);
+	  
+      if (node1.next == node2 || node2.next == node1) 
+      { 
+          node1.prev.next = node2; 
+          node2.prev = node1.prev; 
+		  
+          node2.next.next = node1;
+          node1.next = node2.next; 
+		  
+          node1.prev = node2; 
+          node2.next = node1; 
+      } 
+      else 
+      { 
+          node2.prev = node1.prev); 
+          node2.next = node1.next;
+		  
+          node1.next.prev = node2; 
+          previousNode1.setNext(y);
+		  
+          node1.prev = node2.prev; 
+          node1.next = node2.next;
+		  
+          node2.next.prev = node1; 
+          node2.prev.next = node1; 
+      }  
+*/
 
+/*
+	  Node temp = getNode(Index1); 
+      Node temp2 = getNode(Index2); 
+      getNode(Index1) = temp2; 
+      getNode(Index2) = temp; 
+*/
+	}
+	
+	
+	
+	public Node getNode(int index)throws IndexOutOfBoundsException
+	{
+		if(index > size)
+		{
+			throw new IndexOutOfBoundsException("Index is too large");
+		}
+		
+		if(index == 0)
+		{
+			return head;
+		}
+		
+		if(index == size)
+		{
+			return tail;
+		}
+		
+		else
+		{
+			Node curr = head;
+			int i = 0
 
+			while(curr.next != null && i < index)
+			{
+				curr = curr.next;
+				i++;
+			}
+			
+			return curr;
+		}
+		
 	}
 
+	public void print()
+	{
+		Node curr = head; 
+
+        while (currNode != null) 
+		{ 
+            System.out.print(curr.data + " "); 
+            curr = curr.next; 
+		}
+	}
+	
+	public void printBackwards()
+	{
+		Node curr = tail; 
+
+        while (curr != null) 
+		{ 
+            System.out.print(curr.data + " "); 
+            curr = curr.prev; 
+		}
+	}
+	
+	public void getCustomerSublist (String customerId)
+	
 
 	private class Node
 	{
